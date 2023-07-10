a63b143414939a14ca8792c6aa399b43c2a728b6
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2023-07-10 16:07:15 +0200

    2 - creation des pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageHomeComponent,
+    PageMyPlantsComponent,
+    PageAdminComponent,
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
