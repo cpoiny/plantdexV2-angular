004d41b2498f60a33d53ae3341eaccb38e8f8517
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2023-07-11 10:10:02 +0200

    3 - mise en place routing + classe css active

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // si un user arrive sur localhost:4200 on le redirige vers localhost:4200/home
+  { path: 'home', component: PageHomeComponent },
+  { path: 'my-plants', component: PageMyPlantsComponent },
+  { path: 'admin', component: PageAdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
