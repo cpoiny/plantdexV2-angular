0154c01b46fa9ae6656fee396ea1f718cb0306d4
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2023-07-13 16:47:52 +0200

    6.1 - recuperation valeur checkbox coché

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -7,4 +7,14 @@ import { Component, Input } from '@angular/core';
 })
 export class SideBarComponent {
   @Input() categoriesRecupDeLaHome!: string[];
+
+  onCheckCategory(e: Event) {
+    console.log(e);
+    // Récupérer la catégorie coché
+    // Savoir si la checkbox est cochée ou non
+
+    const target = e.target as HTMLInputElement;
+    console.log('Valeur de la checkbox', target.value);
+    console.log('Est elle cochée ? : ', target.checked);
+  }
 }
